bbbc52086117c05742859fe69cc0629b5528918b
michaelArvidsson <michael.arvidsson@gmail.com>
2020-10-08 11:43:22 +0200

    DB POST

--- DBK_test.php
+++ DBK_test.php
@@ -10,21 +10,43 @@
   <h1>TOMTENS DATABAS 2020</h1>
   <?php
 
-      // Function to debug 
-      function debug($o){
-      echo '<pre>';
-      print_r($o);
-      echo '</pre>';
-      }
+  // Function to debug 
+  function debug($o)
+  {
+    echo '<pre>';
+    print_r($o);
+    echo '</pre>';
+  }
 
+  debug($_POST);
 
-      $pdo = new PDO('mysql:dbname=a18micar_dbk2020;host=localhost', 'sqllab', 'Tomten2009')
+  echo '<ul>';
+  $pdo = new PDO('mysql:dbname=a18micar_dbk2020;host=localhost', 'sqllab', 'Tomten2009');
 
-      foreach($pdo->query( 'SELECT * FROM Verktyg;' ) as $row){
-      debug($row);
-      }
-
+  foreach ($pdo->query('SELECT * FROM Verktyg;') as $row) {
+    echo '<li>' . $row['VerktygsID'] . ' ' . $row['Namn'] . ' ' . $row['Pris'] . ' ' . $row['Magistatus'] . '</li>';
+  }
+  echo '</ul>';
 
   ?>
+  <div style="background-color: greenyellow; width:500px; margin:auto">
+    <form action="DBK_test.php" method="post">
+
+      <h3>Välj verktyg</h3>
+      <select size='1' name='Verktyg'>
+        <?php
+        foreach ($pdo->query('SELECT * FROM Verktyg;') as $row) {
+
+          echo '<option value=' . $row['Namn'] . ' >' . $row['VerktygsID'] . '</option>';
+        }
+
+
+        ?>
+      </select>
+      <input type="submit" value="Visa Magistatus">
+
+    </form>
+  </div>
 </body>
+
 </html>
